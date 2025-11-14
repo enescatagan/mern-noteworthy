6aa13569707b584e270a8791b126676425e4f778
Enes Atağan <enescatagan@gmail.com>
2025-11-14 23:33:29 +0300

    Update Controllers

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,6 +10,9 @@ const PORT = process.env.PORT || 5001;
 
 connectDB();
 
+// Middleware to parse JSON bodies
+app.use(express.json());
+
 app.use("/api/notes", notesRouter);
 
 app.listen(PORT, () => {
